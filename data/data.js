9179fbc7787476bedb76799b88737d322a648e1d
Harshal890 <pardeshiharshal90@gmail.com>
2022-06-14 16:07:18 +0530

    Adding all the product data

---
+++ data/data.js
@@ -0,0 +1,27 @@
+let data = JSON.parse(localStorage.getItem("productData")) || [];
+
+let form = document.querySelector("form");
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  let obj = {
+    img: form["img"].value,
+    det: form["det"].value,
+    Mprice: form["Mprice"].value,
+    Dprice: form["Dprice"].value,
+    Limg: form["Limg"].value,
+    ml: form["ml"].value,
+    ins: form["ins"].value,
+    simg2: form["simg2"].value,
+    simg3: form["simg3"].value,
+    simg4: form["simg4"].value,
+    simg5: form["simg5"].value,
+    simg6: form["simg6"].value,
+    filter: form["filter"].value,
+    sort: form["sort"].value,
+  };
+
+  data.push(obj);
+  localStorage.setItem("productData", JSON.stringify(data));
+
+  location.reload();
+});
