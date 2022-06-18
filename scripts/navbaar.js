6ab5148f85449dbd7984e85ba2bafb0478d62880
Harshal890 <pardeshiharshal90@gmail.com>
2022-06-19 00:13:27 +0530

    Added all functionality parts to cart page

--- scripts/navbaar.js
+++ scripts/navbaar.js
@@ -18,7 +18,7 @@ window.onclick = function (event) {
 
 function cartData(){
   let data = JSON.parse(localStorage.getItem("cartproduct"));
-  console.log(data);
+  // console.log(data);
   let cart_qty = document.getElementById('cart-item-qty');
   let congrats_minis = document.getElementById("free-prod-msg");
   let free_minis = document.getElementById("free-minis");
@@ -27,11 +27,9 @@ function cartData(){
   cart_item(data,cart_qty);
   minis_msg(data,congrats_minis,free_minis);
   subtotal(data,cart_total_amount);
-  cart_products(data,mm_container);
-
-  let cart_cont = document.getElementById("cart-cont");
-  document.getElementById("cart-close").addEventListener("click", mm_remove_cart(cart_cont));
-
+  cart_products(data,mm_container,cart_total_amount);
+  // let cart_cont = document.getElementById("cart-cont");
+  // document.getElementById("cart-close").addEventListener("click", mm_remove_cart(cart_cont));
 }
 
 function cart_item(data,cart_qty) {
@@ -40,10 +38,9 @@ function cart_item(data,cart_qty) {
 
 function minis_msg(data,congrats_minis,free_minis){
   if (data.length === 1) {
-    //***
+   
     congrats_minis.innerHTML = "Buy 1 more item to get 2 free minis";
   } else if (data.length >= 2) {
-    //***
     congrats_minis.innerHTML =
       "Congratulations! You Get 2 D&K MINI Products For FREE";
     free_minis.innerText = "PLEASE SELECT YOUR FREE MINIS";
@@ -52,94 +49,63 @@ function minis_msg(data,congrats_minis,free_minis){
 
 function subtotal(data,cart_total_amount){
   if (data.length !== 0) {
-    //***
     let sum = 0;
-
     data.forEach(function (el) {
-      //***
-
-      sum += el.Dprice * Number(el.qty);
+      sum += Number(el.Dprice) * Number(el.qty);
     });
-
     cart_total_amount.innerText = `Rs : ${sum}`;
   }
 };
 
-function cart_products(data,mm_container){
-  
-
+function cart_products(data,mm_container,cart_total_amount){
   if (data.length === 0) {
-    //***
     empty_cart(mm_container);
   } else {
     data.forEach(function (el, index) {
-      //***
-
       let div1 = document.createElement("div");
-
       let div2 = document.createElement("div");
       div2.setAttribute("id", "bag-product");
       div2.addEventListener("click", function () {
         mm_show_prod();
       });
-
       let div3 = document.createElement("div");
       div3.setAttribute("id", "prod-pr-qty");
-
       let div4_qty = document.createElement("div");
       div4_qty.setAttribute("id", "prod-qty");
-
       let div5 = document.createElement("div");
       div5.setAttribute("id", "mm-subtract-qty");
-
       let div6 = document.createElement("div");
-
       let div7 = document.createElement("div");
       div7.setAttribute("id", "mm-add-qty");
-
       let img = document.createElement("img");
       img.src = el.simg2;
-
       let content = document.createElement("div");
       content.setAttribute("id", "mm-xyz");
       content.innerText = el.det;
-
       let ml = document.createElement("p");
       ml.innerText = `${el.ml}ml`;
-
       let cancel = document.createElement("p");
       cancel.innerText = "X";
       cancel.setAttribute("id", "mm-remove");
       cancel.addEventListener("click", function () {
         remove_prod(index,data);
       });
-
       div5.innerHTML = "-";
-
       div6.innerHTML = el.qty;
-
       div7.innerHTML = "+";
-
       let price = document.createElement("p");
       price.setAttribute("id", "mm-price");
       price.innerText = `Rs : ${Number(el.Dprice)}`;
-
       div5.addEventListener("click", function () {
-        reduce(el, div6);
+        reduce(data,el, div6,cart_total_amount);
       });
-
       div7.addEventListener("click", function () {
-        add(el, div6);
+        add(data,el, div6,cart_total_amount);
       });
-
       div2.append(img, content, ml, cancel);
-
       div4_qty.append(div5, div6, div7);
-
       div3.append(div4_qty, price);
-
       div1.append(div2, div3);
-
       mm_container.append(div1);
     });
   }
@@ -151,27 +117,25 @@ function empty_cart(mm_container) {
 
 function remove_prod(index,data) {
   event.preventDefault();
-
-  data.splice(index, 1); //***
-
-  localStorage.setItem("cartproduct", JSON.stringify(data)); //***
+  data.splice(index, 1);
+  localStorage.setItem("cartproduct", JSON.stringify(data));
   window.location.reload();
 }
 
-function reduce(el, div6) {
+function reduce(data, el, div6,cart_total_amount) {
   if (div6.innerHTML >= 2) {
     div6.innerHTML--;
     el.qty = div6.innerHTML;
-    subtotal();
-    localStorage.setItem("cartproduct", JSON.stringify(data)); //***
+    subtotal(data,cart_total_amount);
+    localStorage.setItem("cartproduct", JSON.stringify(data));
   }
 }
 
-function add(el, div6) {
+function add(data,el, div6,cart_total_amount) {
   div6.innerHTML++;
   el.qty = div6.innerHTML;
-  subtotal();
-  localStorage.setItem("cartproduct", JSON.stringify(data)); //***
+  subtotal(data,cart_total_amount);
+  localStorage.setItem("cartproduct", JSON.stringify(data));
 }
 
 function mm_remove_cart(cart_cont) {
